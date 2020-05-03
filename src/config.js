bbe4d3e0dd55deb5dc471786aa313eea252de444
Alan Baldwin <baldwingaming1979@gmail.com>
2020-05-03 20:55:55 +0100

    unhid file for heroku

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    weatherStack: { 
+        baseUrl: 'http://api.weatherstack.com',
+        key: '4fa9592f02c48edb9511a782c2d67100'
+    },
+    mapBox: {
+        key: 'pk.eyJ1IjoiYmFsZHdpbmdhbWluZyIsImEiOiJjazh1bWUxdmkwYWxiM2dva2JkdXU2NWswIn0.nbRfhvmrp36c5ebujiVeqw',
+        baseUrl: 'https://api.mapbox.com'
+    }
+}
+
+module.exports = config;
